a2b7508082d5fa0aac46d47e154f7406ace0d47b
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-09-13 20:39:27 +0300

    select breed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import { fetchBreeds } from "./cat-api";
+import { fetchBreeds, fetchCatByBreeds } from './cat-api';
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 // start
-select.style.display = "none";
-error.style.display = "none";
+select.style.display = 'none';
+error.style.display = 'none';
 // show loader
-loader.style.display = "block";
+loader.style.display = 'block';
 
 fetchBreeds()
     .then(breeds => {
         select.innerHTML = breeds
             .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
-            .join("");
-        select.style.display = "block";
+            .join('');
+        select.style.display = 'block';
     })
-    .catch(() => error.style.display = "block")
+    .catch(() => (error.style.display = 'block'))
     .finally(() => {
-        loader.style.display = "none";
-    })
+        loader.style.display = 'none';
+    });
 
+// вибір породи
+select.addEventListener('change', () => {
+    const breedId = select.value;
+    loader.style.display = 'block';
+    catInfo.style.display = 'none';
+    error.style.display = 'none';
+
+    fetchCatByBreeds(breedId)
+        .then(data => {
+            const cat = data[0];
+            const breed = cat.breeds[0];
+
+            catInfo.innerHTML =
+                `<img src = "${cat.url}" alt ="${breed.name}" width = "300">
+            <h2>${breed.name}</h2>
+            <p>${breed.description}</p>
+            <p><b>Temperament:</b> ${breed.temperament}</p>`;
+            catInfo.style.display = "block";
+        })
+        .catch(() => {
+            error.style.display = "block";
+        })
+        .finally(() => {
+            loader.style.display = "none";
+        });
+});
